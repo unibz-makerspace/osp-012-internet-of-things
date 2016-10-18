34a4cf7b037b4877cdf89ac89eee41d16d633c3d
Julian Sanin <sanin89julian@gmail.com>
2016-10-18 20:51:44 +0200

    Added first example

---
+++ sketch_01_random_graph/config.h
@@ -0,0 +1,54 @@
+// Adafruit invests time and resources providing this open source code.
+// Please support Adafruit and open source hardware by purchasing
+// products from Adafruit!
+//
+// Written by Todd Treece for Adafruit Industries
+// Copyright (c) 2016 Adafruit Industries
+// Licensed under the MIT license.
+//
+// All text above must be included in any redistribution.
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/************************ Adafruit IO Config *******************************/
+
+// visit io.adafruit.com if you need to create an account,
+// or if you need your Adafruit IO key.
+#define IO_USERNAME    "your_username"
+#define IO_KEY         "your_key"
+
+/******************************* WIFI **************************************/
+
+// the AdafruitIO_WiFi client will work with the following boards:
+//   - HUZZAH ESP8266 Breakout -> https://www.adafruit.com/products/2471
+//   - Feather HUZZAH ESP8266 -> https://www.adafruit.com/products/2821
+//   - Feather M0 WiFi -> https://www.adafruit.com/products/3010
+//   - Feather WICED -> https://www.adafruit.com/products/3056
+
+#define WIFI_SSID       "your_ssid"
+#define WIFI_PASS       "your_pass"
+
+// comment out the following two lines if you are using fona or ethernet
+// #include "AdafruitIO_WiFi.h"
+// AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
+
+
+/******************************* FONA **************************************/
+
+// the AdafruitIO_FONA client will work with the following boards:
+//   - Feather 32u4 FONA -> https://www.adafruit.com/product/3027
+
+// uncomment the following two lines for 32u4 FONA,
+// and comment out the AdafruitIO_WiFi client in the WIFI section
+// #include "AdafruitIO_FONA.h"
+// AdafruitIO_FONA io(IO_USERNAME, IO_KEY);
+
+/**************************** ETHERNET ************************************/
+
+// the AdafruitIO_Ethernet client will work with the following boards:
+//   - Ethernet FeatherWing -> https://www.adafruit.com/products/3201
+#include "AdafruitIO_Ethernet.h"
+AdafruitIO_Ethernet io(IO_USERNAME, IO_KEY);
+
+#endif // CONFIG_H
---
+++ sketch_01_random_graph/sketch_01_random_graph.ino
@@ -0,0 +1,97 @@
+/*
+ * A project to introduce the IOT to highschool students in collaboration with
+ * makerspace - Faculty of Computer Science at the Free University of
+ * Bozen-Bolzano.
+ * 
+ *                                                                         
+ *    m  a  k  e  r  s  p  a  c  e  .  i  n  f  .  u  n  i  b  z  .  i  t  
+ *                                                                         
+ *   8888888888888888888888888888888888888888888888888888888888888888888888
+ *                                                                         
+ *                  8                                                      
+ *                  8                                                      
+ *   YoYoYo. .oPYo. 8  .o  .oPYo. YoYo. .oPYo. 8oPYo. .oPYo. .oPYo. .oPYo. 
+ *   8' 8' 8 .oooo8 8oP'   8oooo8 8  `  Yb..`  8    8 .oooo8 8   `  8oooo8 
+ *   8  8  8 8    8 8 `b.  8.  .  8      .'Yb. 8    8 8    8 8   .  8.  .  
+ *   8  8  8 `YooP8 8  `o. `Yooo' 8     `YooP' 8YooP' `YooP8 `YooP' `Yooo' 
+ *                                             8                           
+ *                                             8                           
+ *                                                                         
+ *   8888888888888888888888888888888888888888888888888888888888888888888888
+ *                                                                         
+ *    c  o  m  p  u  t  e  r    s  c  i  e  n  c  e    f  a  c  u  l  t  y 
+ *                                                                         
+ *                                                                         
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Julian Sanin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/************************** Configuration ***********************************/
+
+// edit the config.h tab and enter your Adafruit IO credentials
+// and any additional configuration needed for WiFi, cellular,
+// or ethernet clients.
+#include "config.h"
+
+/************************ Example Starts Here *******************************/
+
+// set up the 'graph' feed
+AdafruitIO_Feed * graph = io.feed("graph");
+
+void setup() {
+	// use serial port for printing messages
+	Serial.begin(115200);
+	while (!Serial);
+
+	// try to connect to io.adafruit.com
+	io.connect();
+	for (int i = 1; i <= 3; i++) {
+		if (io.status() < AIO_CONNECTED) {
+			Serial.print(".");
+			delay(i * 500);
+		}
+	}
+	Serial.print(' ');
+	Serial.println(io.statusText());
+
+	// if analog input pin 0 is unconnected, random analog
+	// noise will cause the call to randomSeed() to generate
+	// different seed numbers each time the sketch runs.
+	// randomSeed() will then shuffle the random function.
+	randomSeed(analogRead(0));
+}
+
+void loop() {
+	// io.run(); is required for all sketches.
+	// it should always be present at the top of your loop
+	// function. it keeps the client connected to
+	// io.adafruit.com, and processes any incoming data.
+	io.run();
+
+	// save a random value to the 'graph' feed on Adafruit IO
+	long value = random(0, 100);
+	Serial.println(value);
+	graph->save(random(0, 100));
+
+	// wait one second (1000 milliseconds == 1 second)
+	delay(1000);
+}
